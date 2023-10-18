2d9ba2c76efcec5580345b995f47279c9948d151
youhaiou <zegina@qq.com>
2023-10-18 18:02:51 +0800

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,12 @@
 // import Login from "../pages/login";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
+import Publish from "@/pages/Publish";
 
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from '@/components/AuthRoute'
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
 
 // 配置路由实例
 
@@ -14,6 +17,11 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <AuthRoute> <Layout /></AuthRoute>,
+        children: [
+            { path: "home", element: <Home /> },
+            { path: "article", element: <Article /> },
+            { path: "publish", element: <Publish /> }
+        ]
     },
     { path: "/login", element: <Login /> },
 ]);

324c4dd775192a6a6cc3ee3688115ae94f54bd0f
youhaiou <zegina@qq.com>
2023-10-20 14:41:09 +0800

    打包及路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,16 @@
 // import Login from "../pages/login";
 import Layout from "@/pages/layout";
 import Login from "@/pages/login";
-import Publish from "@/pages/Publish";
+// import Publish from "@/pages/Publish";
 
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from '@/components/AuthRoute'
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
+// import Home from "@/pages/Home";
+// import Article from "@/pages/Article";
+import { lazy, Suspense } from 'react' // 优化-路由懒加载
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
+const Home = lazy(() => import('@/pages/Home')) 
 
 // 配置路由实例
 
@@ -18,10 +22,16 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <AuthRoute> <Layout /></AuthRoute>,
+        // children: [
+        //     { index:true, element: <Home /> },
+        //     { path: "article", element: <Article /> },
+        //     { path: "publish", element: <Publish /> }
+        // ]
         children: [
-            { index:true, element: <Home /> },
-            { path: "article", element: <Article /> },
-            { path: "publish", element: <Publish /> }
+            { index: true, element: (<Suspense fallback={'加载中'}><Home /></Suspense>) },
+            { path: "article", element: (<Suspense fallback={'加载中'}><Article /></Suspense>) },
+            { path: "publish", element: (<Suspense fallback={'加载中'}><Publish /></Suspense>) }
+           
         ]
     },
     { path: "/login", element: <Login /> },
